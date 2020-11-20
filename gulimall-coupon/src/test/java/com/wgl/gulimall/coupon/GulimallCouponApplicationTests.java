e79c7c525f629edd4878c2c2e4a8c88aeca64b39
wangguoli <910792937@qq.com>
2020-11-20 16:50:08 +0800

    项目初始结构创建

---
+++ gulimall-coupon/src/test/java/com/wgl/gulimall/coupon/GulimallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wgl.gulimall.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ gulimall-ware/src/test/java/com/wgl/guilimall/ware/GulimallWareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wgl.guilimall.ware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallWareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
